93c968f94ac3e1f3be706c6b7c33543c571bdb69
BaiLiping <blp_engineer@outlook.com>
2021-04-18 10:13:02 +0800

    new

--- hopper/Plot.py
+++ hopper/Plot.py
@@ -5,28 +5,39 @@ import math
 import pickle
 from tqdm import tqdm
 import gym
+import statistics 
 
 from Normal import episode_number
 from Normal import average_over
 from Normal import evaluation_episode_number
 from Normal import measure_length
 
-evaluation_reward_record_Normal=pickle.load(open( "evaluation_normal_record.p", "rb"))
-reward_record_Normal_average=pickle.load(open( "normal_average_record.p", "rb"))
 
-reward_record_single=pickle.load(open( "single_record.p", "rb"))
-evaluation_reward_record_single=pickle.load(open( "evaluation_single_record.p", "rb"))
-reward_record_single_average=pickle.load(open('single_average_record.p','rb'))
+braket_size=50
 
-average_Normal=sum(evaluation_reward_record_Normal)/evaluation_episode_number
-average_single=sum(evaluation_reward_record_single)/evaluation_episode_number
+normal_record=pickle.load(open("normal_record.p","rb"))
+normal_average=[]
+normal_standard_deviation=[]
+for j in range(len(normal_record)-braket_size+1):
+	braket_normal=normal_record[j:j+braket_size]
+	normal_mean=statistics.mean(braket_normal)
+	normal_average.append(normal_mean)
+	normal_standard_deviation.append(statistics.stdev(braket_normal, xbar = normal_mean))
+
+normal_average=np.array(normal_average)
+normal_standard_deviation=np.array(normal_standard_deviation)
+
+evaluation_reward_record_normal=pickle.load(open( "evaluation_normal_record.p", "rb"))
+average_normal=sum(evaluation_reward_record_normal)/evaluation_episode_number
+
 
 #plot
-x=range(len(measure_length))
+x=range(len(normal_average))
 plt.figure(figsize=(13,7))
-plt.plot(x,reward_record_Normal_average,label='Normal Training \n Evaluation Average: \n %s' %average_Normal,color='black')
-plt.plot(x,reward_record_single_average,label='MultiAgents Roll Out Training \n Evaluation Average: \n %s' %average_single,color='magenta')
+plt.plot(x,normal_average,label='MultiAgent Training \n Evaluation Average: \n %s' %average_normal,color='black')
+plt.fill_between(x, normal_average-normal_standard_deviation, normal_average+normal_standard_deviation,color='gray',alpha=0.3)
 plt.xlabel('Episodes Number',fontsize='large')
 plt.ylabel('Episode Reward',fontsize='large')
-plt.legend(loc='lower right',ncol=1, borderaxespad=0,prop={'size': 12})
-plt.savefig('hopper.png')
+plt.legend(loc='upper left',ncol=1, borderaxespad=0,prop={'size': 12})
+plt.savefig('Hopper.png')
+
